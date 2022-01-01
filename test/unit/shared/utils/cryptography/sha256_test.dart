80497e0e36e487064ff5737c0516800113d8ff35
dpajak99 <37120969+dpajak99@users.noreply.github.com>
2022-01-01 22:36:35 +0100

    Refactor: Unification of AES256 algorithm along KIRA ecosystem. NetworkUtils fixes

--- test/unit/shared/utils/cryptography/sha256_test.dart
+++ test/unit/shared/utils/cryptography/sha256_test.dart
@@ -8,7 +8,7 @@ void main() {
   // Expected Values of tests
   String expectedEncryptedString = '51c1657f3cf552b0318bdbd50ac1a2a912f2681deb171fbf4d18e84e84c38750';
 
-  group('Tests of SHA256 algorithm', () {
+  group('Tests of Sha256.encrypt() algorithm', () {
     test('Should correctly encrypt given string via SHA256 algorithm', () async {
       expect(
         Sha256.encrypt(actualStringToEncrypt).toString(),
