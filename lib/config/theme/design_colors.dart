6051f5cb09d1d9aff5b44f3e28d0a08cdc8a09a2
dpajak99 <37120969+dpajak99@users.noreply.github.com>
2022-01-20 15:44:15 +0100

    Feature: Added assets for UI

---
+++ lib/config/theme/design_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DesignColors {
+  static Color brandBlue = const Color(0xFF298DFF);
+  static Color brandPurpleLight = const Color(0xFF4264F2);
+  static Color brandPurpleDark = const Color(0xFF344AE6);
+
+  static Color grayScalePrimary = const Color(0xFF100D32);
+  static Color grayScaleAdditional = const Color(0xFF201E48);
+  static Color grayScaleSecondary = const Color(0xFF343261);
+  static Color grayScaleTeritary = const Color(0xFF4E4C71);
+  static Color grayScaleQuaternary = const Color(0xFF6F6C99);
+  static Color grayScaleQuinary = const Color(0xFF6489B4);
+  static Color grayScaleSenary = const Color(0xFFE3EFFA);
+  static Color grayScaleSeptenary = const Color(0xFFF4FAFF);
+
+}
