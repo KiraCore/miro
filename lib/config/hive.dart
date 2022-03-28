4de7dd0a8c57cf5c2a6d08ebd127f34af09eb5e6
dpajak99 <dominik00801@gmail.com>
2022-03-28 16:47:51 +0200

    Bugfix: App is now launching in Firefox private window
    Hive is not initializing in Firefox private window, in that case new CacheRepository has been created to handle temporary cache (after closing private window cache is erased).

--- lib/config/hive.dart
+++ lib/config/hive.dart
@@ -1,7 +1,9 @@
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:miro/blocs/specific_blocs/lists/balance_list_bloc.dart';
 
 Future<void> initHive() async {
+  await Hive.initFlutter();
+
   await Hive.openBox<String>('configuration');
   await Hive.openBox<bool>(BalanceListBloc.favouriteCacheWorkspace);
 }
--- lib/infra/cache/app_config_cache.dart
+++ lib/infra/cache/app_config_cache.dart
@@ -1,17 +1,19 @@
-import 'package:hive/hive.dart';
+import 'package:miro/config/locator.dart';
+import 'package:miro/infra/cache/cache_manager.dart';
 
 class AppConfigCache {
-  final Box<String> cache = Hive.box<String>('configuration');
+  final String boxName = 'configuration';
+  final CacheManager _cacheManager = globalLocator<CacheManager>();
 
-  String? getConfig(String key, {String? defaultValue}) {
-    return cache.get(key, defaultValue: defaultValue);
+  String getConfig(String key, {String? defaultValue}) {
+    return _cacheManager.get<String>(boxName: boxName, key: key, defaultValue: defaultValue ?? '');
   }
 
   void updateConfig(String key, String? value) {
     if (value == null) {
-      cache.delete(key);
+      _cacheManager.delete<String>(boxName: boxName, key: key);
     } else {
-      cache.put(key, value);
+      _cacheManager.add<String>(boxName: boxName, key: key, value: value);
     }
   }
 }
