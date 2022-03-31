b186679659a03b741470e00dd8971873517bd9b8
asmodat <asmodat@gmail.com>
2022-03-31 20:42:02 +0000

    make sure to load environment variables

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
+. /etc/profile || echo "WARNING: Failed to load environment variables"
 set -x
+
 
 echo "INFO: Starting build & analyze process..."
 
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+. /etc/profile || echo "WARNING: Failed to load environment variables"
 set -x
 
 echo "INFO: Starting package publishing process..."
