41fa940446c26bb79b5c8f54d26eb21896e25a98
dpajak99 <dominik00801@gmail.com>
2022-07-13 11:25:35 +0200

    123

--- lib/views/widgets/generic/visualiser_map/visualiser_canvas.dart
+++ lib/views/widgets/generic/visualiser_map/visualiser_canvas.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:miro/views/widgets/generic/visualiser_map/country_model.dart';
+import 'package:miro/views/widgets/generic/visualiser_map/visualiser_painter.dart';
 
 class VisualiserCanvas extends StatefulWidget {
   final List<CountryModel> countryModels;
@@ -16,6 +17,11 @@ class VisualiserCanvas extends StatefulWidget {
 class _VisualiserCanvas extends State<VisualiserCanvas> {
   @override
   Widget build(BuildContext context) {
-    return
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.yellow,
+      child: CustomPaint(painter: VisualiserPainter(countryModels: countryModels)),
+    );
   }
 }
--- lib/views/widgets/generic/visualiser_map/visualiser_painter.dart
+++ lib/views/widgets/generic/visualiser_map/visualiser_painter.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:miro/views/widgets/generic/visualiser_map/country_model.dart';
 
 class VisualiserPainter extends CustomPainter {
+  final List<CountryModel> countryModels;
+
+  VisualiserPainter({
+    required this.countryModels,
+  });
+
   @override
   void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-      ..style = PaintingStyle.fill
-      ..color = Colors.blue
-      ..isAntiAlias = true;
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4.0
+      ..color = Colors.indigo;
+
+    canvas.drawCircle(
+      const Rect.fromCenter(center: Offset(0, 0), width: 20, height: 20),
+      paint,
+    );
   }
 
   @override
