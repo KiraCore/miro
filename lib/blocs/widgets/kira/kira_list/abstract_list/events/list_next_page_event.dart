83f99db7788cc5abd6543eefed7859679b5e8826
dpajak99 <37120969+dpajak99@users.noreply.github.com>
2023-07-11 15:22:30 +0200

    Bugfix: List reload on network change
    Recently merged "dp-feature/paginated-list" (#38) branch had a bug related to keeping the same page number, after changing the network. This means if a list page number was changed and later the network was switched, the page number remained the same. This makes sense if the networks have the same results, however, if the network has fewer items in the list, the page numbers don't get updated or restarted, and the view responds with “No results”.
    
    List of changes:
    - created a new parameter in the ListNextPageEvent event to store reload status information
    - updated the "jumpToTop" parameter in the ListNextPageEvent event handler, based on the reload status, to return to the first page after a network change

--- lib/blocs/widgets/kira/kira_list/abstract_list/events/list_next_page_event.dart
+++ lib/blocs/widgets/kira/kira_list/abstract_list/events/list_next_page_event.dart
@@ -1,3 +1,14 @@
 import 'package:miro/blocs/widgets/kira/kira_list/abstract_list/a_list_event.dart';
 
-class ListNextPageEvent extends AListEvent {}
+class ListNextPageEvent extends AListEvent {
+  final bool afterReloadBool;
+
+  const ListNextPageEvent({
+    this.afterReloadBool = false,
+  });
+
+  @override
+  List<Object?> get props => <Object?>[
+    afterReloadBool,
+  ];
+}
