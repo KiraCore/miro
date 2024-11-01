702d4feba7b66eec93b56c0ea2d2a77d6761996a
Mykyta Sadchenko <31825654+muknta@users.noreply.github.com>
2024-11-01 04:46:25 +0200

    Bugfix: ukex denomination duplication
    Replace denomination ukex of -6 precision with KEX. Add [networkTokenDenominationModel] in case where the denominations don't exist for token (such as v1/ukex). Examples: ukex -> KEX, v1/ukex -> v1/KEX
    
    List of changes:
    - add method in token_alias_model.dart to replace denomination that contains precision prefix "u" in [networkTokenDenominationModel]
    - add [networkTokenDenominationModel] to TokenAliasModel.local which means there are no denominations for token

--- lib/shared/models/tokens/token_alias_model.dart
+++ lib/shared/models/tokens/token_alias_model.dart
@@ -17,21 +17,22 @@ class TokenAliasModel extends Equatable {
 
   factory TokenAliasModel.local(String name) {
     return TokenAliasModel(
-      name: name,
+      name: _removePrecisionPrefixFromDenomination(name),
       defaultTokenDenominationModel: TokenDenominationModel(name: name, decimals: 0),
+      networkTokenDenominationModel: TokenDenominationModel(name: _removePrecisionPrefixFromDenomination(name), decimals: 6),
     );
   }
 
   factory TokenAliasModel.fromDto(TokenAlias tokenAlias) {
     TokenDenominationModel networkTokenDenominationModel = TokenDenominationModel(
-      name: tokenAlias.symbol,
+      name: _removePrecisionPrefixFromDenomination(tokenAlias.symbol),
       decimals: tokenAlias.decimals,
     );
     TokenDenominationModel defaultTokenDenominationModel =
         tokenAlias.denoms.isNotEmpty ? TokenDenominationModel(name: tokenAlias.denoms.first, decimals: 0) : networkTokenDenominationModel;
 
     return TokenAliasModel(
-      name: tokenAlias.name,
+      name: _removePrecisionPrefixFromDenomination(tokenAlias.name),
       icon: tokenAlias.icon,
       networkTokenDenominationModel: networkTokenDenominationModel,
       defaultTokenDenominationModel: defaultTokenDenominationModel,
@@ -44,6 +45,22 @@ class TokenAliasModel extends Equatable {
       networkTokenDenominationModel,
     };
     return availableTokenDenominationModelSet.toList();
+  }
+
+  static String _removePrecisionPrefixFromDenomination(String name) {
+    if (name.toLowerCase().startsWith('u')) {
+      // NOTE: can be `ukex`
+      return name.substring(1).toUpperCase();
+    } else if (name.contains('/')) {
+      // NOTE: can be `v1/ukex`
+      List<String> nameParts = name.split('/');
+      String nameWithoutDenomination = nameParts.sublist(0, nameParts.length - 1).join('/');
+      String denomination = nameParts.last;
+      if (denomination.toLowerCase().startsWith('u')) {
+        return '$nameWithoutDenomination/${denomination.substring(1).toUpperCase()}';
+      }
+    }
+    return name;
   }
 
   @override
